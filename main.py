a0bede0c7a32bcc1329532f1bad953e33e69b44b
Jeremiah <jeremiah.fallin@gmail.com>
2017-05-21 22:50:04 -0700

    Updated paths to be relative.

--- main.py
+++ main.py
@@ -10,71 +10,65 @@ def main():
     # api = RiotAPI('RGAPI-826d5283-e20d-4ceb-b890-dc2ce5e91fb9')
     get_matches(api)
     get_matchIDs(api)
+    retrieve_matches(api)
+    get_summonerIDs(api)
     get_stats_from_matches()
-    retrieve_matches()
-    get_summonerIDs()
 
 
-class APIError(Exception):
-    """
-    Args:
-        message (str): the error message
-        error_code (int): the HTTP error code that was received
-    """
-    def __init__(self, message, error_code):
-        super().__init__(message)
-        self.message = message
-        self.error_code = error_code
-
-
+#This pulls the matches from the Riot API.
 def get_matches(api):
-    with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data', 'matchIDs.txt'), 'r+') as file:
+    with open(os.path.join(os.path.dirname(__file__), 'training_data', 'matchIDs.txt'), 'r+') as file:
         for h in file:
             match = h.rstrip()
-            #print(match)
             try:
-                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches', match + '.txt'), 'x') as file:
+                with open(os.path.join(os.path.dirname(__file__), 'training_data/matches', match + '.txt'), 'x') as file:
+                    print('Pulling match #' + match)
                     h = api.get_match_by_id(match)
                     if not h == 0:
                         json.dump(h, file, indent=4)
-                        with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches_for_training/', match + '.txt.'), 'a+') as f:
+                        with open(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match + '.txt.'), 'a+') as f:
                             json.dump(h, f, indent=4)
                     else:
                         file.close()
-                        os.remove(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches', match + '.txt'))
+                        os.remove(os.path.join(os.path.dirname(__file__), 'training_data/matches', match + '.txt'))
             except OSError as e:
                 if e.errno != 17:
                     raise
                 pass
+    get_summonerIDs(api)
 
 
-def get_summonerIDs():
-    for filename in os.listdir('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches/'):
-        with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches/', filename), 'r') as j:
+#This pulls the summoners from our matches and writes them to a file.
+def get_summonerIDs(api):
+    for filename in os.listdir(os.path.dirname(__file__), 'training_data/matches/'):
+        with open(os.path.join(os.path.dirname(__file__), 'training_data/matches/', filename), 'r') as j:
             i = json.load(j)
-            #print(filename)
+            print('Pulling summoners from match: ' + filename)
             for m in i['participantIdentities']:
-                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data', 'summonerIDs.txt'), 'a+') as file:
+                with open(os.path.join(os.path.dirname(__file__), 'training_data', 'summonerIDs.txt'), 'a+') as file:
                     for line in file:
                         if str(m['player']['summonerId']) in line:
                             break
                     else:
                         file.write(str(m['player']['summonerId']) + '\n')
+    get_matchIDs(api)
 
 
+#This pulls the match history of a summoner from the Riot API.
 def get_matchIDs(api):
     apir = RiotAPI('RGAPI-dd446914-d130-4817-a1c2-bad4d08ed858', 'na')
-    with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data', 'summonerIDs.txt'), 'r+') as file:
+    with open(os.path.join(os.path.dirname(__file__), 'training_data', 'summonerIDs.txt'), 'r+') as file:
         for line in file:
             summoner = line.rstrip()
+            print('Pulling summoner #' + match)
             try:
-                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/summoners/', summoner + '.txt'), 'x') as f:
+                with open(os.path.join(os.path.dirname(__file__), 'training_data/summoners/', summoner + '.txt'), 'x') as f:
                     t = apir.get_matches_by_id(summoner)
                     if t != 0:
                         json.dump(t, f, indent=4)
                         for m in t['matches']:
                             if m['queue'] == 'TEAM_BUILDER_RANKED_SOLO' or m['queue'] == 'RANKED_FLEX_SR':
-                                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data',
+                                with open(os.path.join(os.path.dirname(__file__), 'training_data',
                                                        'matchIDs.txt'), 'a+') as file:
                                     for line in file:
                                         if str(m['matchId']) in line:
@@ -83,7 +77,7 @@ def get_matchIDs(api):
                                         file.write(str(m['matchId']) + '\n')
                     else:
                         f.close()
-                        os.remove(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/summoners/', summoner + '.txt'))
+                        os.remove(os.path.join(os.path.dirname(__file__), 'training_data/summoners/', summoner + '.txt'))
             except OSError as e:
                 if e.errno != 17:
                     raise
@@ -92,16 +86,17 @@ def get_matchIDs(api):
                 if e == 'matches':
                     raise
                 pass
+    get_matches(api)
 
-
-def retrieve_matches():
-    for summoner in os.listdir('Z:/Homebrew Programs/Python/WinEstimate/training_data/summoners/'):
-        with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/summoners/', summoner), 'r+') as f:
-            #print(summoner)
+#This pulls the ranked matches from a summoner IDs match history and writes them to a file..
+def retrieve_matches(api):
+    for summoner in os.listdir(os.path.dirname(__file__), 'training_data/summoners/'):
+        with open(os.path.join(os.path.dirname(__file__), 'training_data/summoners/', summoner), 'r+') as f:
+            print('Pulling matches from summoner #' + summoner)
             t = json.load(f)
             for m in t['matches']:
                 if m['queue'] == 'TEAM_BUILDER_RANKED_SOLO' or m['queue'] == 'RANKED_FLEX_SR':
-                    with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data', 'matchIDs.txt'), 'a+') as file:
+                    with open(os.path.join(os.path.dirname(__file__), 'training_data', 'matchIDs.txt'), 'a+') as file:
                         for line in file:
                             if str(m['matchId']) in line:
                                 break
@@ -109,25 +104,27 @@ def retrieve_matches():
                             file.write(str(m['matchId']) + '\n')
 
 
+
+#This takes the copy of the matches and puts them in a CSV file that I was planning on running through a neural net.
 def get_stats_from_matches():
-    for match in os.listdir('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches_for_training/'):
-        with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches_for_training/', match), 'r+') as f:
+    for match in os.listdir(os.path.dirname(__file__), 'training_data/matches_for_training/'):
+        with open(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match), 'r+') as f:
             t = json.load(f)
             for m in t['participants']:
-                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/input_layer', 'input.txt'), 'a+') as file:
+                with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'input.txt'), 'a+') as file:
                     if m != 10:
                         file.write(str(m['championId']) + ',' + str(m['stats']['kills']) + ',' + str(m['stats']['deaths']) + ',' + str(m['stats']['assists']) + ',')
                     else:
                         file.write(str(m['championId']) + ',' + str(m['stats']['kills']) + ',' + str(m['stats']['deaths']) + ',' + str(m['stats']['assists']))
-            with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/input_layer', 'input.txt'), 'a+') as file:
+            with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'input.txt'), 'a+') as file:
                 file.write('\n')
             for m in t['teams']:
-                with open(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/input_layer', 'output.txt'), 'a+') as file:
+                with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'output.txt'), 'a+') as file:
                     if m['win'] == 'Win' and m == 0:
                         file.write('1\n')
                     else:
                         file.write('0\n')
-        os.remove(os.path.join('Z:/Homebrew Programs/Python/WinEstimate/training_data/matches_for_training/', match))
+        os.remove(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match))
 
 
 if __name__ == "__main__":
