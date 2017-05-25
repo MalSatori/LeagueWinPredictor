cbc85f1ae5546b1a6e748a8a926149feb402e2fd
Jeremiah <jeremiah.fallin@gmail.com>
2017-05-25 02:22:34 -0700

    Updated paths to be relative.

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def main():
     api = RiotAPI('RGAPI-dd446914-d130-4817-a1c2-bad4d08ed858')
     # api = RiotAPI('RGAPI-826d5283-e20d-4ceb-b890-dc2ce5e91fb9')
     get_matches(api)
+    get_summonerIDs(api)
     get_matchIDs(api)
     retrieve_matches(api)
-    get_summonerIDs(api)
     get_stats_from_matches()
 
 
 def hold():
-    while not RiotAPI('RGAPI-dd446914-d130-4817-a1c2-bad4d08ed858').can_make_request():
+    while not RiotAPI('key').can_make_request():
         time.sleep(1)
 
 #This pulls the matches from the Riot API.
@@ -33,6 +33,14 @@ def get_matches(api):
                         json.dump(h, file, indent=4)
                         with open(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match + '.txt.'), 'a+') as f:
                             json.dump(h, f, indent=4)
+                        for m in h['participantIdentities']:
+                            with open(os.path.join(os.path.dirname(__file__), 'training_data', 'summonerIDs.txt'),
+                                      'a+') as file:
+                                for line in file:
+                                    if str(m['player']['summonerId']) in line:
+                                        break
+                                else:
+                                    file.write(str(m['player']['summonerId']) + '\n')
                     else:
                         file.close()
                         os.remove(os.path.join(os.path.dirname(__file__), 'training_data/matches', match + '.txt'))
@@ -43,29 +51,13 @@ def get_matches(api):
     get_summonerIDs(api)
 
 
-#This pulls the summoners from our matches and writes them to a file.
-def get_summonerIDs(api):
-    for filename in os.listdir(os.path.dirname(__file__), 'training_data/matches/'):
-        with open(os.path.join(os.path.dirname(__file__), 'training_data/matches/', filename), 'r') as j:
-            i = json.load(j)
-            print('Pulling summoners from match: ' + filename)
-            for m in i['participantIdentities']:
-                with open(os.path.join(os.path.dirname(__file__), 'training_data', 'summonerIDs.txt'), 'a+') as file:
-                    for line in file:
-                        if str(m['player']['summonerId']) in line:
-                            break
-                    else:
-                        file.write(str(m['player']['summonerId']) + '\n')
-    get_matchIDs(api)
-
-
 #This pulls the match history of a summoner from the Riot API.
 def get_matchIDs(api):
-    apir = RiotAPI('RGAPI-dd446914-d130-4817-a1c2-bad4d08ed858', 'na')
+    apir = RiotAPI('key', 'na')
     with open(os.path.join(os.path.dirname(__file__), 'training_data', 'summonerIDs.txt'), 'r+') as file:
         for line in file:
             summoner = line.rstrip()
-            print('Pulling summoner #' + match)
+            print('Pulling summoner #' + summoner)
             try:
                 with open(os.path.join(os.path.dirname(__file__), 'training_data/summoners/', summoner + '.txt'), 'x') as f:
                     hold()
@@ -93,45 +85,6 @@ def get_matchIDs(api):
                     raise
                 pass
     get_matches(api)
-
-#This pulls the ranked matches from a summoner IDs match history and writes them to a file..
-def retrieve_matches(api):
-    for summoner in os.listdir(os.path.dirname(__file__), 'training_data/summoners/'):
-        with open(os.path.join(os.path.dirname(__file__), 'training_data/summoners/', summoner), 'r+') as f:
-            print('Pulling matches from summoner #' + summoner)
-            t = json.load(f)
-            for m in t['matches']:
-                if m['queue'] == 'TEAM_BUILDER_RANKED_SOLO' or m['queue'] == 'RANKED_FLEX_SR':
-                    with open(os.path.join(os.path.dirname(__file__), 'training_data', 'matchIDs.txt'), 'a+') as file:
-                        for line in file:
-                            if str(m['matchId']) in line:
-                                break
-                        else:
-                            file.write(str(m['matchId']) + '\n')
-
-
-
-#This takes the copy of the matches and puts them in a CSV file that I was planning on running through a neural net.
-def get_stats_from_matches():
-    for match in os.listdir(os.path.dirname(__file__), 'training_data/matches_for_training/'):
-        with open(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match), 'r+') as f:
-            t = json.load(f)
-            for m in t['participants']:
-                with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'input.txt'), 'a+') as file:
-                    if m != 10:
-                        file.write(str(m['championId']) + ',' + str(m['stats']['kills']) + ',' + str(m['stats']['deaths']) + ',' + str(m['stats']['assists']) + ',')
-                    else:
-                        file.write(str(m['championId']) + ',' + str(m['stats']['kills']) + ',' + str(m['stats']['deaths']) + ',' + str(m['stats']['assists']))
-            with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'input.txt'), 'a+') as file:
-                file.write('\n')
-            for m in t['teams']:
-                with open(os.path.join(os.path.dirname(__file__), 'training_data/input_layer', 'output.txt'), 'a+') as file:
-                    if m['win'] == 'Win' and m == 0:
-                        file.write('1\n')
-                    else:
-                        file.write('0\n')
-        os.remove(os.path.join(os.path.dirname(__file__), 'training_data/matches_for_training/', match))
-
 
 if __name__ == "__main__":
     main()
